eca6065a03268eeac63dd0adcd973f6c249d609a
Muhammad Taaha <96954628+TSTRIVER@users.noreply.github.com>
2024-09-13 20:38:59 +0530

    form fixing

--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { ValidationError, useForm } from "@formspree/react";
 
 const ContactSection = () => {
-    return (
-      <>
-        <h2 className="text-5xl font-bold">Contact me</h2>
-        <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-          <form>
+  const [state, handleSubmit] = useForm("mwpejbjk");
+
+  return (
+    <>
+      <h2 className="text-3xl md:text-5xl font-bold">Contact me</h2>
+      <div className="mt-8 p-8 rounded-md bg-white bg-opacity-50 w-96 max-w-full">
+        {state.succeeded ? (
+          <p className="text-gray-900 text-center">Thanks for your message !</p>
+        ) : (
+          <form onSubmit={handleSubmit}>
             <label for="name" className="font-medium text-gray-900 block mb-1">
               Name
             </label>
@@ -27,6 +33,12 @@ const ContactSection = () => {
               id="email"
               className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
             />
+            <ValidationError
+              className="mt-1 text-red-500"
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
             <label
               for="email"
               className="font-medium text-gray-900 block mb-1 mt-8"
@@ -38,13 +50,21 @@ const ContactSection = () => {
               id="message"
               className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
             />
-            <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+            <ValidationError
+              className="mt-1 text-red-500"
+              errors={state.errors}
+            />
+            <button
+              disabled={state.submitting}
+              className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 "
+            >
               Submit
             </button>
           </form>
-        </div>
-      </>
-    );
-  };
-  
-  export default ContactSection
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ContactSection;
